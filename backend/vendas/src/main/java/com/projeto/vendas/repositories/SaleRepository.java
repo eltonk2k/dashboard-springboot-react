227b4acb592fe477e0680b8d1f631c5b2ad2eb5f
eltonk2k <elieltonpb1@gmail.com>
2021-05-06 12:03:31 -0300

    Pagination

---
+++ backend/vendas/src/main/java/com/projeto/vendas/repositories/SaleRepository.java
@@ -0,0 +1,10 @@
+package com.projeto.vendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projeto.vendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+	
+
+}
