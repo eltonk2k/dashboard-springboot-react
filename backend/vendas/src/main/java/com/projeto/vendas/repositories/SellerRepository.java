f7beb23654e29d752f81edb3346b976edd807a55
eltonk2k <elieltonpb1@gmail.com>
2021-05-05 23:27:57 -0300

    Layers

---
+++ backend/vendas/src/main/java/com/projeto/vendas/repositories/SellerRepository.java
@@ -0,0 +1,10 @@
+package com.projeto.vendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projeto.vendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+	
+
+}
